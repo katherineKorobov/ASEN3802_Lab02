93c0866a4631f58117431ce51f51aa9ee8595f6f
Katherine Korobov <katya.korobov@gmail.com>
2026-03-24 11:52:57 -0600

    start task 3

--- main.m
+++ main.m
@@ -13,13 +13,14 @@ current = [240,290,237,285,203]*10^-3; % [A]
 k = [130, 130, 115, 115, 16.2]; % [W/(mC)]
 density = [2810, 2810, 8500, 8500, 8000];
 c_p = [960, 960, 380, 380, 500];
+M = [3.09, 1.69, 6.468, 6.65, 19.78];
 
 
 % Create structs to hold data
 for i = 1:length(files)
     material(i) = struct("data", readmatrix("data\" + files{i}), ...
                          "Voltage", voltage(i), "Current", current(i), ...
-                         "k", k(i), "alpha", k(i)/(density(i)*c_p(i)) ,"t0", 0,"Hexp", 0,"Han", 0 );
+                         "k", k(i), "alpha", k(i)/(density(i)*c_p(i)) ,"t0", 0,"Hexp", 0,"Han", 0, "M", M(i));
 end
 
 material_names = ["Aluminum - 25V", "Aluminum - 30V", ...
@@ -36,9 +37,10 @@ end
 
 %% Plotting 
 plotSteadyState = 0;
-plotTransientModel_1A = 1;
-plotTransientModel_1B = 1;
+plotTransientModel_1A = 0;
+plotTransientModel_1B = 0;
 plotTransientModel_2 = 0;
+plotAllModels = 1;
 
 if plotSteadyState
     figure();
@@ -97,4 +99,40 @@ if plotTransientModel_1B
         hold off;
         
     end
+end
+
+if plotTransientModel_2
+    model_name = "Model II";
+    for i = 1:length(material)
+        
+        % Getting the variables
+        H = material(i).Hexp;
+        T0 = material(i).t0;
+        alpha = material(i).alpha;
+        M = material(i).M;
+
+      % II func
+        u_II = @(x_val, t_val) T0 + H*x_val + sum(arrayfun(@(n) ((2*(M-H)*sin(((2*n-1)*pi/2))) / (L*((2*n-1)*pi/(2*L))^2)) * sin(((2*n-1)*pi/(2*L))*x_val) * exp(-(((2*n-1)*pi/(2*L))^2)*alpha*t_val), 1:10));
+       
+        % Plotting
+
+        figure();
+        hold on;
+
+        plotTransientTemperatureSolution(x, u_II, material(i).data, material_names(i), model_name);
+        hold off;
+        
+    end
+end
+
+
+%% Task 3
+
+if plotAllModels
+    
+    for i = length(materials)
+        
+
+    end
+
 end
