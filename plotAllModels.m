18075fff4770c09b345d1a115f48a4d9846bef94
KAVI2978 <149836305+KAVI2978@users.noreply.github.com>
2026-03-24 21:34:33 -0600

    Add files via upload

--- plotAllModels.m
+++ plotAllModels.m
@@ -1,26 +1,29 @@
 function plotAllModels(x, u, material_data, material_name, model_name)
-    t_max = max(material_data(:,1));
-    t = linspace(0, t_max, t_max); % [s]
+    t = material_data(:,1); % [s]
+    T_analytical = zeros(size(t));
+
+    for j = 1:length(t)            
+        T_analytical(j) = u(x, t(j));
+    end
     
+    % Model Temperature
+    plot(t, T_analytical, "-", 'LineWidth', 1.5); % Plot analytical data
+    
+    % Experimental Temperature
+    if model_name == "Model III"
+        plot(t, material_data(:, 9), 'r-', 'LineWidth', 1.5); % Plotting experimental data
+        
+        % Shaded Error Region
+        p = patch([t; flip(t)], [material_data(:,9)+2; flip(material_data(:,9)-2)], [0.5, 0.5, 0.5]);
+        p.FaceAlpha = 0.2; % transparency
+        p.EdgeColor = [0.5, 0.5, 0.5]; % grey border
+    end
+    title(material_name + "'s Model Comparison at 8th Thermo-Couple");
+    grid on;
 
-     for i = 1:length(x)      
-         T_analytical = zeros(size(t));
-
-        for j = 1:length(t)            
-            T_analytical(j) = u(x(i), t(j));
-
-        end
-
-        plot(t, T_analytical, 'LineWidth', 1.5); % Plot analytical data
-        if model_name == "Model III"
-            plot(material_data(:, 1), material_data(:, 9), 'r-', 'LineWidth', 1.5); % Plotting experimental data
-        end
-        title(material_name + "'s Model Comparison at 8th Thermo-Couple");
-        grid on;
-
-     end
-     legend("Model IA", "Model IB", "Model II", "Model III", "Experimental Transient Behavior", "Location",'southeast');
-     xlabel("Times [s]");
-     ylabel('Temperature [{\circ}C]')
-  
+    legend("Model IA", "Model IB", "Model II", "Model III", "Experimental Transient Behavior", "Error", "Location",'southeast');
+    xlabel("Times [s]");
+    ylabel('Temperature [{\circ}C]')
+    
+    hold off;
 end
